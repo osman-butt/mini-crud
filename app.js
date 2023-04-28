a725732650a75146fbbf13c90fa993a0003774ef
osman-butt <butt.osman@gmail.com>
2023-04-28 11:30:23 +0200

    added get data from API function

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 
 window.addEventListener("load", initApp);
 
-function initApp() {
+const endpoint =
+  "https://mini-crud-kea-default-rtdb.europe-west1.firebasedatabase.app";
+const query = "cards";
+
+async function initApp() {
   console.log("initApp: app.js is running 🎉");
+  const data = await getFlashCards();
+  console.log(data);
+}
+
+async function getFlashCards() {
+  console.log("---getFlashCards()---");
+  const response = await fetch(`${endpoint}/${query}.json`);
+  const data = await response.json();
+  return data;
 }
